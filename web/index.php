4d73481989c915d1b23fcc72fad66dafdd719c8d
Michał Sokołowski <michal.sokolowski@smtsoftware.com>
2015-09-26 12:40:21 +0200

    REST list, item methods added + data mock

--- web/index.php
+++ web/index.php
@@ -6,32 +6,41 @@ $app          = new Silex\Application();
 $app['debug'] = 'true';
 
 $app->get('/v1/item/{number}', function ($number) use ($app) {
-    $itemStub = [
-        'title'      => 'Nius',
-        'content'    => 'costam',
-        'link'       => 'http://onet.pl',
-        'lang'       => 'PL',
-        'created_at' => date('Y-m-d'),
-    ];
+    try {
 
-    return json_encode($itemStub);
+        $itemStub = [
+            'title'      => 'Nius',
+            'content'    => 'costam',
+            'link'       => 'http://onet.pl',
+            'lang'       => 'PL',
+            'created_at' => date('Y-m-d'),
+        ];
+
+        return json_encode($itemStub);
+    } catch (\Exception $e) {
+        return json_encode(['error' => 'Problem with processing']);
+    }
 });
 
 $app->get('/v1/list/{page}', function ($page) use ($app) {
-    $itemStub   = [
-        'title'      => 'Nius',
-        'content'    => 'costam',
-        'link'       => 'http://onet.pl',
-        'lang'       => 'PL',
-        'created_at' => date('Y-m-d'),
-    ];
+    try {
+        $itemStub = [
+            'title'      => 'Nius',
+            'content'    => 'costam',
+            'link'       => 'http://onet.pl',
+            'lang'       => 'PL',
+            'created_at' => date('Y-m-d'),
+        ];
 
-    $collection = [];
-    for ($i = 0; $i < 10; $i++) {
-        $collection[] = $itemStub;
+        $collection = [];
+        for ($i = 0; $i < 10; $i++) {
+            $collection[] = $itemStub;
+        }
+
+        return json_encode($collection);
+    } catch (\Exception $e) {
+        return json_encode(['error' => 'Problem with processing']);
     }
-
-    return json_encode($collection);
 });
 
 $app->run(); 
