9dad51c7db4f406c820208ae3a9c8cafed26c146
Mikołaj Kopras <mikolaj.kopras@smtsoftware.com>
2015-09-26 12:38:45 +0200

    Stub for news fetcher

--- web/index.php
+++ web/index.php
@@ -2,10 +2,15 @@
 
 require_once __DIR__.'/../vendor/autoload.php'; 
 
-$app = new Silex\Application(); 
+$app = new Silex\Application();
+// $app['debug'] = true;
+
+$app['news_fetcher'] = $app->share(function () {
+    return new \News\RSS\Fetcher();
+});
 
 $app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
+    return 'Hello '.$app->escape($name);
 }); 
 
 $app->run(); 
