6cb4766f6e4bcd526b15ae1eb1249ccfa6dd2908
Mikołaj Kopras <mikolaj.kopras@smtsoftware.com>
2015-09-26 13:32:01 +0200

    Replaced fetcher stub with TVN Fetcher

--- web/index.php
+++ web/index.php
@@ -3,8 +3,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app                 = new Silex\Application();
-$app['news_fetcher'] = $app->share(function () {
-    return new \News\RSS\Fetcher();
+$app['rss_client'] = $app->share(function () {
+    return new \Vinelab\Rss\Rss();
+});
+
+$app['news_fetcher'] = $app->share(function () use ($app) {
+    return new \News\RSS\TVNFetcher($app['rss_client']);
 });
 
 $app->get('/v1/item/{number}', function ($number = 0) use ($app) {
