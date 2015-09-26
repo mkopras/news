1f5de2d01df91008c0e4d7cc0f52a8ad67e28cd3
Michał Sokołowski <michal.sokolowski@smtsoftware.com>
2015-09-26 12:34:38 +0200

    REST list, item methods added + data mock

--- web/index.php
+++ web/index.php
@@ -1,11 +1,37 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php'; 
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Silex\Application(); 
+$app          = new Silex\Application();
+$app['debug'] = 'true';
 
-$app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
-}); 
+$app->get('/v1/item/{number}', function ($number) use ($app) {
+    $itemStub = [
+        'title'      => 'Nius',
+        'content'    => 'costam',
+        'link'       => 'http://onet.pl',
+        'lang'       => 'PL',
+        'created_at' => date('Y-m-d'),
+    ];
+
+    return json_encode($itemStub);
+});
+
+$app->get('/v1/list/{page}', function ($page) use ($app) {
+    $itemStub   = [
+        'title'      => 'Nius',
+        'content'    => 'costam',
+        'link'       => 'http://onet.pl',
+        'lang'       => 'PL',
+        'created_at' => date('Y-m-d'),
+    ];
+
+    $collection = [];
+    for ($i = 0; $i < 10; $i++) {
+        $collection[] = $itemStub;
+    }
+
+    return json_encode($collection);
+});
 
 $app->run(); 
